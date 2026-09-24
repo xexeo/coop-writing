9588aa0dd8bfefbc5e2a4f2fd8175e588f49f2d0
Geraldo Xexeo <gxexeo@gmail.com>
2026-09-24 07:20:51 -0300

    test: add l3build regression suite for engines, modes, hyperref, and core classes

---
+++ build.lua
@@ -0,0 +1,23 @@
+module = "coop-writing"
+
+sourcefiles = {"coop-writing.dtx", "coop-writing.ins"}
+unpackfiles = {"coop-writing.ins"}
+installfiles = {"coop-writing.sty"}
+
+checkengines = {"pdftex", "luatex"}
+stdengine = "pdftex"
+checkruns = 2
+testfiledir = "testfiles"
+
+typesetfiles = {"coop-writing.dtx"}
+typesetexe = "pdflatex"
+
+textfiles = {
+  "README.md",
+  "README-pt-br.md",
+  "changes.md",
+  "new.md",
+  "LICENSE",
+  "COMPATIBILITY.md",
+  "DEPENDENCIES.md"
+}
